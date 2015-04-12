12b8fb3efbd0fa04cae52bbdeaccb4fc5a233847
copper111 <artpg@yandex.ru>
2015-04-12 23:48:35 +0300

    Инфраструктура поиска призывателей подготовка

--- GamesHistory/views.py
+++ GamesHistory/views.py
@@ -1,7 +1,7 @@
 __author__ = 'artpg'
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from riotwatcher import RiotWatcher
 
 
 def searchsummoner(request):
@@ -9,10 +9,17 @@ def searchsummoner(request):
     )
 
 def gameshistory(request):
-    if 'summoner' in request.GET:
+    if ('summoner' in request.GET):
         summoner_name = request.GET['summoner']
+        region = request.GET['region']
     else:
         summoner_name = 'You submitted an empty form.'
+
+    w = RiotWatcher('12934b95-1914-42bc-b092-208bb64c7793')
+    me = w.get_summoner(name=summoner_name, region=region)
+    mh = w.get_match_history(me['id'], region=region, begin_index=14, end_index=15)
+
+
     return render(request, 'gameshistory.html', {
-        'summoner_name': summoner_name}
+        'summoner_name': summoner_name, 'gson': me, 'mh':mh}
     )
